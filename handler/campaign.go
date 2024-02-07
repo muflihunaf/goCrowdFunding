5d2ff906f2c452a518676fe74780cb5fe418a950
MuflihunDev <firmanedam19@gmail.com>
2024-02-07 14:13:11 +0700

    Campaign Handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,32 @@
 package handler
+
+import (
+	"net/http"
+	"startup/campaign"
+	"startup/helper"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userId, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.FindCampaign(userId)
+	if err != nil {
+		response := helper.ApiResponse("Error to get Data", http.StatusBadRequest, "failed", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.ApiResponse("Success", http.StatusOK, "Success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
